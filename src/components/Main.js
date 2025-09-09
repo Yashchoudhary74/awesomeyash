952709aca233eb586c023e38ca61e0177929b7d9
Yash Choudhary <yashchoudhary1101@gmail.com>
2025-09-09 22:30:13 +0530

    All file to be added.

---
+++ src/components/Main.js
@@ -0,0 +1,61 @@
+import imageData from '../api/imageData';
+import { useEffect, useState } from 'react';   
+import '../styles/style.css'
+export const Main = () => {
+
+    useEffect(()=>{
+        setTimeout(() => {
+            setLoading({display: 'none'})
+        }, 4000);
+    },[])
+
+    const [loading, setLoading] = useState({display: 'flex'});
+
+    document.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+    });
+
+    return (
+
+
+
+        <>
+            <div className='main'>
+                <h1>Photo Gallery</h1>
+                <span> Yash's photos</span>
+
+                <div className='image-gallery'>
+                    {
+
+
+                        imageData.map((data, index) => {
+                            return (
+                                <div  className='image-card' key={index} >
+                                    <img src={data.src} />
+                                </div>
+                            )
+                        })
+                    }
+
+                </div>
+
+                <span className='footer-text' >Designed by Yash Choudhary</span>
+
+
+
+            </div>
+
+            <div  style={loading} className='loading-screen'>
+                <div className='loader'>
+
+                    <span className='loading-text'>YC</span>
+                    
+
+                </div>
+                
+
+            </div>
+        </>
+
+    );
+}
